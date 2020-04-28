bb4d3e138b7784c2da659b8bf5fde39a2d9b8724
Gorak Nikita <gorak.n.s@edu.mirea.ru>
2020-04-28 18:17:06 +0300

    Lab2 complete

---
+++ lab2/src/main/java/com/example/lab2/BasicTechnology.kt
@@ -0,0 +1,3 @@
+package com.example.lab2
+
+open class BasicTechnology(val graphic: String, val name: String, val helptext: String) {}
---
+++ lab2/src/main/java/com/example/lab2/MyFragment.kt
@@ -0,0 +1,29 @@
+package com.example.lab2
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.fragment_my.*
+
+class MyFragment constructor(page: String): Fragment() {
+
+    var item: String = page
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        fragmentTextView.text = item
+        Picasso.get().load("https://raw.githubusercontent.com/wesleywerner/ancient-tech/02decf875616dd9692b31658d92e64a20d99f816/src/images/tech/" + item).into(myImageView)
+    }
+
+}
